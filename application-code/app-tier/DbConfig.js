de8336d9282c3c295e54ba59fd40b0e092467f32
Shubham Sakhare <66584634+shubhamcancode@users.noreply.github.com>
2025-08-20 17:16:28 +0530

    Update DbConfig.js
    changes 0

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : '10.0.3.51',
+    DB_USER : 'admin',
+    DB_PWD : 'Shubham1234',
+    DB_DATABASE : 'webappdb'
 });
